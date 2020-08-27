c00a5d352f135694353492f60bb487a4e6008378
Chandra Khatri <chandrak@uber.com>
2020-08-27 15:29:27 -0700

    Auto generation and integration of Sphinx docs with travis

--- make_docs.sh
+++ make_docs.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-touch docs/_build/html/.nojekyll
-cd $TRAVIS_BUILD_DIR/docs
-make  html
-cd $TRAVIS_BUILD_DIR
+#touch docs/_build/html/.nojekyll
+#cd $TRAVIS_BUILD_DIR/docs
+#make  html
+#cd $TRAVIS_BUILD_DIR
+make -C docs/ html
