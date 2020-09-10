b0d93c27e5389677077d09fe1a781d3e211f3ae2
Chandra Khatri <chandrak@uber.com>
2020-09-10 11:21:46 -0700

    Updating license

--- docs/conf.py
+++ docs/conf.py
@@ -10,6 +10,23 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+
+"""
+Copyright (c) 2019-2020 Uber Technologies, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 from plato import __version__
 import os
 import sys
